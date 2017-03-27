2830fcdbc45b475577bd24d6d2d064ac2b1516b4
Andrea Zonca <code@andreazonca.com>
2017-03-27 16:51:52 -0700

    first version of python material

---
+++ python/README.md
@@ -0,0 +1,9 @@
+# swcarpentry_inflammation_hpc
+
+1.5 hours lesson to introduce principles of parallel computing
+
+builds up on the Python Novice Inflammation dataset
+
+Please open an issue on the Github repository if something is not working or not clear.
+
+See: <https://github.com/zonca/swcarpentry_inflammation_hpc>
---
+++ python/analyze_inflammation.py
@@ -0,0 +1,50 @@
+import numpy
+
+# configure matplotlib with a non-interactive backend so that it can run on the workers
+import matplotlib
+matplotlib.use("agg")
+
+import matplotlib.pyplot
+
+def analyze(filename):
+    """Create plot from inflammation data
+    
+    Load inflammation data from csv, create plots of average, max and min
+    
+    Parameters
+    -----------
+    filename : str
+         filename of csv data
+    """
+
+    data = numpy.loadtxt(fname=filename, delimiter=',')
+
+    fig = matplotlib.pyplot.figure(figsize=(10.0, 3.0))
+
+    axes1 = fig.add_subplot(1, 3, 1)
+    axes2 = fig.add_subplot(1, 3, 2)
+    axes3 = fig.add_subplot(1, 3, 3)
+
+    axes1.set_ylabel('average')
+    axes1.plot(numpy.mean(data, axis=0))
+
+    axes2.set_ylabel('max')
+    axes2.plot(numpy.max(data, axis=0))
+
+    axes3.set_ylabel('min')
+    axes3.plot(numpy.min(data, axis=0))
+
+    fig.tight_layout()
+    matplotlib.pyplot.savefig(filename.replace("csv", "png"))
+    matplotlib.pyplot.close(fig)
+    
+def detect_problems(filename):
+
+    data = numpy.loadtxt(fname=filename, delimiter=',')
+
+    if numpy.max(data, axis=0)[0] == 0 and numpy.max(data, axis=0)[20] == 20:
+        print('Suspicious looking maxima!')
+    elif numpy.sum(numpy.min(data, axis=0)) == 0:
+        print('Minima add up to zero!')
+    else:
+        print('Seems OK!')
