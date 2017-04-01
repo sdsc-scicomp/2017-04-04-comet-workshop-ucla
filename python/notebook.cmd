c3ae4f69d83654bea01e1c6a3c188257cf66b16d
Andrea Zonca <code@andreazonca.com>
2017-04-01 14:45:31 -0700

    notebook launch slurm script

---
+++ python/notebook.cmd
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name="jupyter-notebook"
+#SBATCH --output="jupyter-notebook.%j.%N.out"
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --export=ALL
+##SBATCH --reservation=SI2016D3Morning
+#SBATCH -t 02:00:00
+
+module load anaconda
+jupyter notebook --no-browser --ip=*
